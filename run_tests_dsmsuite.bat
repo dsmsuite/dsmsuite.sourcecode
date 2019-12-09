4e971ea91846fb6a76410cfe4bc425f6644f09a0
jmuijsenberg <johan.van.den.muijsenberg@gmail.com>
2019-12-09 21:52:35 +0100

    Add batch file to run tests from command line

---
+++ run_tests_dsmsuite.bat
@@ -0,0 +1,14 @@
+vstest.console.exe build\bin\DsmSuite.Analyzer.Util.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.Analyzer.Cpp.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.Analyzer.DotNet.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.Analyzer.Jdeps.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.Analyzer.Model.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.Transformer.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.Analyzer.VisualStudio.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.Common.Util.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.DsmViewer.Application.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.DsmViewer.Reporting.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.DsmViewer.Builder.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.DsmViewer.Model.Test.dll /Settings:test.runsettings
+vstest.console.exe build\bin\DsmSuite.DsmViewer.ViewModel.Test.dll /Settings:test.runsettings
+
