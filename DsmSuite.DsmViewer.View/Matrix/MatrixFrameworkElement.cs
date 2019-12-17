e34293a42b2ae02d0fd781728f7ece0c7b4e379d
jmuijsenberg <johan.van.den.muijsenberg@gmail.com>
2019-12-17 19:17:32 +0100

    Updated performance matrix text drawing

--- DsmSuite.DsmViewer.View/Matrix/MatrixFrameworkElement.cs
+++ DsmSuite.DsmViewer.View/Matrix/MatrixFrameworkElement.cs
@@ -11,6 +11,21 @@ namespace DsmSuite.DsmViewer.View.Matrix
         private static readonly float PixelsPerDip;
         private static readonly RotateTransform TextTransform;
         private static readonly double FontSize = 12;
+        private static readonly GlyphInfo[] m_glyphInfoTable;
+        private static List<ushort> m_glyphIndexesList = new List<ushort>();
+        private static List<double> m_advanceWidthsList = new List<double>();
+
+        private struct GlyphInfo
+        {
+            public readonly ushort Index;
+            public readonly double Width; // Pre-computed with font size for now
+
+            public GlyphInfo(ushort glyphIndex, double width) : this()
+            {
+                Index = glyphIndex;
+                Width = width;
+            }
+        }
 
         static MatrixFrameworkElement()
         {
@@ -19,7 +34,16 @@ namespace DsmSuite.DsmViewer.View.Matrix
             TextColor = new SolidColorBrush(Colors.Black);
             PixelsPerDip = 1.0f;
 
-            TextTransform = new RotateTransform {Angle = 90};
+            m_glyphInfoTable = new GlyphInfo[char.MaxValue];
+            foreach (var kvp in GlyphTypeface.CharacterToGlyphMap)
+            {
+                char c = (char)kvp.Key;
+                var glyphIndex = kvp.Value;
+                double width = GlyphTypeface.AdvanceWidths[glyphIndex] * FontSize;
+                m_glyphInfoTable[c] = new GlyphInfo(glyphIndex, width);
+            }
+
+            TextTransform = new RotateTransform { Angle = 90 };
         }
 
         protected void DrawRotatedText(DrawingContext dc, Point location, string text, double maxWidth)
@@ -32,28 +56,27 @@ namespace DsmSuite.DsmViewer.View.Matrix
 
         protected void DrawText(DrawingContext dc, Point location, string text, double maxWidth)
         {
-            double totalWidth = 0;
-
-            List<ushort> glyphIndexesList = new List<ushort>();
-            List<double> advanceWidthsList = new List<double>();
-            foreach (char t in text)
+            if (text.Length > 0)
             {
-                if (totalWidth < maxWidth)
+                double totalWidth = 0;
+
+                m_glyphIndexesList.Clear();
+                m_advanceWidthsList.Clear();
+
+                foreach (char c in text)
                 {
-                    ushort glyphIndex = GlyphTypeface.CharacterToGlyphMap[t];
-                    glyphIndexesList.Add(glyphIndex);
+                    if (totalWidth < maxWidth)
+                    {
+                        var info = m_glyphInfoTable[c];
+                        m_glyphIndexesList.Add(info.Index);
+                        m_advanceWidthsList.Add(info.Width);
 
-                    double width = GlyphTypeface.AdvanceWidths[glyphIndex] * FontSize;
-                    advanceWidthsList.Add(width);
-
-                    totalWidth += width;
+                        totalWidth += info.Width;
+                    }
                 }
-            }
 
-            if (glyphIndexesList.Count > 0)
-            {
                 GlyphRun glyphRun = new GlyphRun(GlyphTypeface, 0, false, FontSize, PixelsPerDip,
-                    glyphIndexesList.ToArray(), location, advanceWidthsList.ToArray(),
+                    m_glyphIndexesList.ToArray(), location, m_advanceWidthsList.ToArray(),
                     null, null, null, null, null, null);
 
                 dc.DrawGlyphRun(TextColor, glyphRun);
