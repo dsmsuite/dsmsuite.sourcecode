1bbe21f2165623f1c27383a7cf3df551ad346636
Johan van den Muijsenberg <johan.van.den.muijsenberg@gmail.com>
2019-12-20 07:02:03 +0100

    Split action data model off from main data model

---
+++ DsmSuite.DsmViewer.Model/Core/DsmActionsDataModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using DsmSuite.DsmViewer.Model.Interfaces;
+
+namespace DsmSuite.DsmViewer.Model.Core
+{
+    public class DsmActionsDataModel
+    {
+        private readonly List<IDsmAction> _actions;
+
+        public DsmActionsDataModel()
+        {
+            _actions = new List<IDsmAction>();
+        }
+
+        public void Clear()
+        {
+            _actions.Clear();
+        }
+
+        public IDsmAction ImportAction(int id, string type, IReadOnlyDictionary<string, string> data)
+        {
+            IDsmAction action = new DsmAction(id, type, data);
+            _actions.Add(action);
+            return action;
+        }
+
+        public IDsmAction AddAction(int id, string type, IReadOnlyDictionary<string, string> data)
+        {
+            IDsmAction action = new DsmAction(id, type, data);
+            _actions.Add(action);
+            return action;
+        }
+
+        public void ClearActions()
+        {
+            _actions.Clear();
+        }
+
+        public IEnumerable<IDsmAction> GetActions()
+        {
+            return _actions;
+        }
+
+        public int GetActionCount()
+        {
+            return _actions.Count;
+        }
+    }
+}
--- DsmSuite.DsmViewer.Model/Core/DsmModel.cs
+++ DsmSuite.DsmViewer.Model/Core/DsmModel.cs
@@ -14,14 +14,14 @@ namespace DsmSuite.DsmViewer.Model.Core
         private readonly MetaDataModel _metaDataModel;
         private readonly DsmElementsDataModel _elementsDataModel;
         private readonly DsmRelationsDataModel _relationsDataModel;
-        private readonly List<IDsmAction> _actions;
+        private readonly DsmActionsDataModel _actionsDataModel;
 
         public DsmModel(string processStep, Assembly executingAssembly)
         {
             _metaDataModel = new MetaDataModel(processStep, executingAssembly);
             _elementsDataModel = new DsmElementsDataModel();
             _relationsDataModel = new DsmRelationsDataModel(_elementsDataModel);
-            _actions = new List<IDsmAction>();
+            _actionsDataModel = new DsmActionsDataModel();
         }
 
         public void LoadModel(string dsmFilename, IProgress<DsmProgressInfo> progress)
@@ -84,26 +84,22 @@ namespace DsmSuite.DsmViewer.Model.Core
 
         public IDsmAction ImportAction(int id, string type, IReadOnlyDictionary<string, string> data)
         {
-            IDsmAction action = new DsmAction(id, type, data);
-            _actions.Add(action);
-            return action;
+            return _actionsDataModel.ImportAction(id, type, data);
         }
 
         public IDsmAction AddAction(int id, string type, IReadOnlyDictionary<string, string> data)
         {
-            IDsmAction action = new DsmAction(id, type, data);
-            _actions.Add(action);
-            return action;
+            return _actionsDataModel.AddAction(id, type, data);
         }
 
         public void ClearActions()
         {
-            _actions.Clear();
+            _actionsDataModel.Clear();
         }
 
         public IEnumerable<IDsmAction> GetActions()
         {
-            return _actions;
+            return _actionsDataModel.GetActions();
         }
 
         public IDsmElement ImportElement(int id, string name, string type, int order, bool expanded, int? parentId)
@@ -266,7 +262,7 @@ namespace DsmSuite.DsmViewer.Model.Core
 
         public int GetActionCount()
         {
-            return _actions.Count;
+            return _actionsDataModel.GetActionCount();
         }
     }
 }
