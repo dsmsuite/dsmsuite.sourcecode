e9afa29b1443c1666f96999dd1ea1aa6c932babf
jmuijsenberg <johan.van.den.muijsenberg@gmail.com>
2019-12-24 19:32:49 +0100

    Add combobox to select sort algortim

--- DsmSuite.DsmViewer.Application/Sorting/AlphabeticalSortAlgorithm.cs
+++ DsmSuite.DsmViewer.Application/Sorting/AlphabeticalSortAlgorithm.cs
@@ -1,16 +1,13 @@
 ﻿using DsmSuite.DsmViewer.Application.Algorithm;
 using DsmSuite.DsmViewer.Model.Core;
 using DsmSuite.DsmViewer.Model.Interfaces;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DsmSuite.DsmViewer.Application.Sorting
 {
-    internal class AlphabeticalSortAlgorithm
+    internal class AlphabeticalSortAlgorithm : ISortAlgorithm
     {
         private readonly IDsmModel _model;
         private readonly DsmElement _element;
