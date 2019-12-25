25e9e96bf40b54a609ebde326784f94ac2164c65
jmuijsenberg <johan.van.den.muijsenberg@gmail.com>
2019-12-25 08:44:39 +0100

    Improved permutation test

--- DsmSuite.DsmViewer.Application.Test/Algorithm/PermutationTest.cs
+++ DsmSuite.DsmViewer.Application.Test/Algorithm/PermutationTest.cs
@@ -8,35 +8,37 @@ namespace DsmSuite.DsmViewer.Application.Test.Algorithm
     public class PermutationTest
     {
         [TestMethod]
-        public void GivenTwoPermutationsAreIdentcalWhenCheckForEqualsRetrunsTrue()
+        public void GivenTwoPermutationsWithIdenticalInputValuesWhenCheckingForEqualityThenTheyAreTheSame()
         {
             Permutation permutation1 = new Permutation(12, 34);
             Permutation permutation2 = new Permutation(12, 34);
             Assert.IsTrue(permutation1.Equals(permutation2));
-        }
-
-        [TestMethod]
-        public void GivenTwoPermutationsAreIdentcalWhenGetHashThenReturnSameValue()
-        {
-            Permutation permutation1 = new Permutation(12, 34);
-            Permutation permutation2 = new Permutation(12, 34);
             Assert.AreEqual(permutation1.GetHashCode(), permutation2.GetHashCode());
         }
 
         [TestMethod]
-        public void GivenTwoPermutationsAreDiffentWhenCheckForEqualsReturnsFalse()
+        public void GivenTwoPermutationsWithIdenticalInputValuesButDifferentOrderWhenCheckingForEqualityThenTheyAreTheSame()
+        {
+            Permutation permutation1 = new Permutation(12, 34);
+            Permutation permutation2 = new Permutation(34, 12);
+            Assert.IsTrue(permutation1.Equals(permutation2));
+            Assert.AreEqual(permutation1.GetHashCode(), permutation2.GetHashCode());
+        }
+
+        [TestMethod]
+        public void GivenTwoPermutationsWithDifferentInputValuesWhenCheckingForEqualityThenTheyAreDifferent()
         {
             Permutation permutation1 = new Permutation(12, 34);
             Permutation permutation2 = new Permutation(12, 56);
             Assert.IsFalse(permutation1.Equals(permutation2));
+            Assert.AreNotEqual(permutation1.GetHashCode(), permutation2.GetHashCode());
         }
 
         [TestMethod]
-        public void GivenTwoPermutationsAreIdentcalWhenGetHashThenReturnDifferentValue()
+        public void GivenOnePermutationsWhenCheckingForEqualityWithNonPermutationObjectThenTheyAreDifferent()
         {
             Permutation permutation1 = new Permutation(12, 34);
-            Permutation permutation2 = new Permutation(12, 56);
-            Assert.AreNotEqual(permutation1.GetHashCode(), permutation2.GetHashCode());
+            Assert.IsFalse(permutation1.Equals(this));
         }
     }
 }
