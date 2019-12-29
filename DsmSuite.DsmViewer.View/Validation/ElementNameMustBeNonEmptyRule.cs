6c651cc2b378f10b67f73d6786d0348e34446cae
Johan van den Muijsenberg <johan.van.den.muijsenberg@gmail.com>
2019-12-29 14:31:45 +0100

    Prepared for input validation

---
+++ DsmSuite.DsmViewer.View/Validation/ElementNameMustBeNonEmptyRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DsmSuite.DsmViewer.View.Validation
+{
+    public class ElementNameMustBeNonEmptyRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            ValidationResult result;
+            string name = value.ToString();
+            if (name.Length == 0)
+            {
+                result = new ValidationResult(false, "Please enter non empty string");
+            }
+            else
+            {
+                result = new ValidationResult(true, null);
+            }
+            return result;
+        }
+    }
+}
---
+++ DsmSuite.DsmViewer.View/Validation/WeightMustBePositiveNumberRule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DsmSuite.DsmViewer.View.Validation
+{
+    public class WeightMustBePositiveNumberRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            ValidationResult result;
+            int weight;
+            if (int.TryParse(value.ToString(), out weight))
+            {
+                result = new ValidationResult(false, "Please enter a valid integer value.");
+            }
+            else
+            {
+                if (weight <= 0)
+                {
+                    result = new ValidationResult(false, "Please enter a positive integer value.");
+                }
+                else
+                {
+                    result = new ValidationResult(true, null);
+                }
+            }
+            return result;
+        }
+    }
+}
