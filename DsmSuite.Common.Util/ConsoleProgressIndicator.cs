45040c2de97333dcca6854ece5c4b817855b554d
Johan van den Muijsenberg <johan.van.den.muijsenberg@gmail.com>
2020-01-20 12:27:20 +0100

    Add console progess indicator

---
+++ DsmSuite.Common.Util/ConsoleProgressIndicator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsmSuite.Common.Util
+{
+    public class ConsoleProgressIndicator
+    {
+        private int _progress;
+
+        public ConsoleProgressIndicator()
+        {
+            _progress = 0;
+        }
+
+        public void Update(ProgressInfo progress)
+        {
+            if (_progress != progress.Progress)
+            {
+                _progress = progress.Progress;
+                Console.Write($"\r {progress.ActionText} progress={progress.Progress:000}%");
+            }
+
+            if (_progress == 100)
+            {
+                Console.Write($"{progress.ActionText} progress={progress.Progress:000}%");
+                Console.Write("Done");
+            }
+        }
+    }
+}
