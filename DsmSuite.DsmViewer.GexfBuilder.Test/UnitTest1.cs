1947911425e0e31bd87c9a52ea04d336aeec4971
jmuijsenberg <amina$1GH>
2020-08-23 09:53:56 +0200

    Update DsmSuite.DsmViewer.GexfBuilder.Test.csproj, AssemblyInfo.cs, and 12 more files...

---
+++ DsmSuite.DsmViewer.GexfBuilder.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DsmSuite.DsmViewer.GexfBuilder.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
