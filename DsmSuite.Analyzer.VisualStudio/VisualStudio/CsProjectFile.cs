658e9c127a3769d4c92fa689a6ab69ddf87f8296
Johan van den Muijsenberg <johan.van.den.muijsenberg@gmail.com>
2020-08-30 17:16:47 +0200

    Find assembly generated by csproj

--- DsmSuite.Analyzer.VisualStudio/VisualStudio/CsProjectFile.cs
+++ DsmSuite.Analyzer.VisualStudio/VisualStudio/CsProjectFile.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using DsmSuite.Analyzer.DotNet.Analysis;
 using DsmSuite.Analyzer.VisualStudio.Settings;
+using DsmSuite.Common.Util;
+using Microsoft.Build.Evaluation;
 
 namespace DsmSuite.Analyzer.VisualStudio.VisualStudio
 {
@@ -16,6 +21,77 @@ namespace DsmSuite.Analyzer.VisualStudio.VisualStudio
 
         public override void Analyze()
         {
+            Project project = OpenProject();
+
+            if (project != null)
+            {
+                foreach (var property in project.AllEvaluatedProperties)
+                {
+                    if (property.Name == "TargetPath")
+                    {
+                        FileInfo fileInfo = new FileInfo(property.EvaluatedValue);
+                        string assemblyFilename = fileInfo.FullName;
+                    }
+                }
+            }
+        }
+
+        private Project OpenProject()
+        {
+            Project project = null;
+            try
+            {
+                string solutionDir = SolutionDir;
+                if (!solutionDir.EndsWith(@"\"))
+                {
+                    solutionDir += @"\";
+                }
+                Dictionary<string, string> globalProperties = new Dictionary<string, string>
+                {
+                    ["Configuration"] = "Release",
+                    ["Platform"] = "x64",
+                    ["SolutionDir"] = solutionDir
+                };
+                project = new Project(ProjectFileInfo.FullName, globalProperties, AnalyzerSettings.ToolsVersion);
+                UpdateConfiguration(project);
+            }
+            catch (Exception e)
+            {
+                Logger.LogException($"Open project failed project={ProjectFileInfo.FullName}", e);
+            }
+            return project;
+        }
+
+        private void UpdateConfiguration(Project project)
+        {
+            foreach (ProjectItem item in project.Items)
+            {
+                if (item.ItemType == "ProjectConfiguration")
+                {
+                    string configuration = null;
+                    string platform = null;
+                    foreach (ProjectMetadata meta in item.Metadata)
+                    {
+                        if (meta.Name == "Configuration")
+                        {
+                            configuration = meta.UnevaluatedValue;
+                        }
+
+                        if (meta.Name == "Platform")
+                        {
+                            platform = meta.UnevaluatedValue;
+                        }
+                    }
+
+                    if ((configuration != null) && (platform != null))
+                    {
+                        project.SetGlobalProperty("Configuration", configuration);
+                        project.SetGlobalProperty("Platform", platform);
+                    }
+
+                }
+            }
+            project.ReevaluateIfNecessary();
         }
     }
 }
