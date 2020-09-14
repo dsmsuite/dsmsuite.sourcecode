8992a79e6d1f15d4b424fc1447e4e9fc8074665f
jmuijsenberg <amina$1GH>
2020-09-14 08:33:42 +0200

    Renamed diff to compare tool and completed functionality to compare dsi files

---
+++ DsmSuite.Analyzer.Diff/Comparer.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DsmSuite.Analyzer.Model.Interface;
+using DsmSuite.Common.Util;
+using Microsoft.SqlServer.Server;
+
+namespace DsmSuite.Analyzer.Diff
+{
+    class Comparer
+    {
+        private readonly IDsiModel _model1;
+        private readonly IDsiModel _model2;
+        private IProgress<ProgressInfo> _progress;
+
+        private readonly HashSet<string> _foundModel1ElementNames = new HashSet<string>();
+        private readonly HashSet<string> _foundModel2ElementNames = new HashSet<string>();
+        private readonly HashSet<string> _missingModel1ElementNames = new HashSet<string>();
+        private readonly HashSet<string> _missingModel2ElementNames = new HashSet<string>();
+        private readonly HashSet<string> _allElementNames = new HashSet<string>();
+
+        private readonly Dictionary<string, HashSet<string>> _foundModel1RelationProviderNames = new Dictionary<string, HashSet<string>>();
+        private readonly Dictionary<string, HashSet<string>> _foundModel2RelationProviderNames = new Dictionary<string, HashSet<string>>();
+        private readonly Dictionary<string, HashSet<string>> _missingModel1RelationProviderNames = new Dictionary<string, HashSet<string>>();
+        private readonly Dictionary<string, HashSet<string>> _missingModel2RelationProviderNames = new Dictionary<string, HashSet<string>>();
+        private readonly Dictionary<string, HashSet<string>> _allRelationProviderNames = new Dictionary<string, HashSet<string>>();
+
+        public Comparer(IDsiModel model1, IDsiModel model2, IProgress<ProgressInfo> progress)
+        {
+            _model1 = model1;
+            _model2 = model2;
+            _progress = progress;
+        }
+
+        public void Compare()
+        {
+            FindElements();
+
+            foreach (string elementName in _allElementNames)
+            {
+                CompareElement(elementName);
+            }
+
+            foreach (string elementName in _allElementNames)
+            {
+                CompareElementRelations(elementName);
+            }
+
+            if (AreIdentical())
+            {
+                Logger.LogUserMessage("Model are identical");
+            }
+            else
+            {
+                Logger.LogUserMessage("Model are different");
+                Logger.LogUserMessage("###################");
+
+                Logger.LogUserMessage("");
+                Logger.LogUserMessage("Elements");
+                Logger.LogUserMessage("--------");
+
+                bool foundElementDeltas = false;
+                if (_missingModel1ElementNames.Count > 0)
+                {
+                    foundElementDeltas = true;
+                    Logger.LogUserMessage("Missing in model 1");
+                    foreach (string missingElementName in _missingModel1ElementNames)
+                    {
+                        Logger.LogUserMessage($" {missingElementName}");
+                    }
+                    Logger.LogUserMessage("");
+                }
+
+                if (_missingModel2ElementNames.Count > 0)
+                {
+                    foundElementDeltas = true;
+                    Logger.LogUserMessage("Missing in model 2");
+                    foreach (string missingElementName in _missingModel2ElementNames)
+                    {
+                        Logger.LogUserMessage($" {missingElementName}");
+                    }
+                    Logger.LogUserMessage("");
+                }
+
+                if (!foundElementDeltas)
+                {
+                    Logger.LogUserMessage(" none");
+                    Logger.LogUserMessage("");
+                }
+
+                Logger.LogUserMessage("Relations");
+                Logger.LogUserMessage("--------");
+
+                bool foundRelationDeltas = false;
+                foreach (string elementName in _allElementNames)
+                {
+                    if (_missingModel1RelationProviderNames[elementName].Count > 0)
+                    {
+                        foundRelationDeltas = true;
+                        Logger.LogUserMessage($"Missing in model 1 for {elementName}");
+                        foreach (string missingTargetName in _missingModel1RelationProviderNames[elementName])
+                        {
+                            Logger.LogUserMessage($" {missingTargetName}");
+                        }
+                        Logger.LogUserMessage("");
+                    }
+
+                    if (_missingModel2RelationProviderNames[elementName].Count > 0)
+                    {
+                        foundRelationDeltas = true;
+                        Logger.LogUserMessage($"Missing in model 2 for {elementName}");
+                        foreach (string missingTargetName in _missingModel2RelationProviderNames[elementName])
+                        {
+                            Logger.LogUserMessage($" {missingTargetName}");
+                        }
+                        Logger.LogUserMessage("");
+                    }
+                }
+
+                if (!foundRelationDeltas)
+                {
+                    Logger.LogUserMessage(" none");
+                    Logger.LogUserMessage("");
+                }
+            }
+        }
+
+        private void FindElements()
+        {
+            foreach (IDsiElement element in _model1.GetElements())
+            {
+                _foundModel1ElementNames.Add(element.Name);
+                _allElementNames.Add(element.Name);
+            }
+
+            foreach (IDsiElement element in _model2.GetElements())
+            {
+                _foundModel2ElementNames.Add(element.Name);
+                _allElementNames.Add(element.Name);
+            }
+        }
+
+        private void CompareElement(string elementName)
+        {
+            if (!_foundModel1ElementNames.Contains(elementName))
+            {
+                _missingModel1ElementNames.Add(elementName);
+            }
+
+            if (!_foundModel2ElementNames.Contains(elementName))
+            {
+                _missingModel2ElementNames.Add(elementName);
+            }
+        }
+
+        private void CompareElementRelations(string consumerName)
+        {
+            _foundModel1RelationProviderNames[consumerName] = new HashSet<string>();
+            _foundModel2RelationProviderNames[consumerName] = new HashSet<string>();
+            _missingModel1RelationProviderNames[consumerName] = new HashSet<string>();
+            _missingModel2RelationProviderNames[consumerName] = new HashSet<string>();
+            _allRelationProviderNames[consumerName] = new HashSet<string>();
+
+            foreach (IDsiRelation relation in _model1.GetRelations())
+            {
+                IDsiElement consumer = _model1.FindElementById(relation.ConsumerId);
+                IDsiElement provider = _model1.FindElementById(relation.ProviderId);
+
+                if (consumer.Name == consumerName)
+                {
+                    _foundModel1RelationProviderNames[consumerName].Add(provider.Name);
+                    _allRelationProviderNames[consumerName].Add(provider.Name);
+                }
+            }
+
+            foreach (IDsiRelation relation in _model2.GetRelations())
+            {
+                IDsiElement consumer = _model1.FindElementById(relation.ConsumerId);
+                IDsiElement provider = _model1.FindElementById(relation.ProviderId);
+
+                if (consumer.Name == consumerName)
+                {
+                    _foundModel2RelationProviderNames[consumerName].Add(provider.Name);
+                    _allRelationProviderNames[consumerName].Add(provider.Name);
+                }
+            }
+
+            foreach (string providerName in _allRelationProviderNames[consumerName])
+            {
+                if (!_foundModel1RelationProviderNames[consumerName].Contains(providerName))
+                {
+                    _missingModel1RelationProviderNames[consumerName].Add(providerName);
+                }
+
+                if (!_foundModel2RelationProviderNames[consumerName].Contains(providerName))
+                {
+                    _missingModel2RelationProviderNames[consumerName].Add(providerName);
+                }
+            }
+        }
+
+        private bool AreIdentical()
+        {
+            bool identical = true;
+
+            if (_missingModel1ElementNames.Count > 0)
+            {
+                identical = false;
+            }
+
+            if (_missingModel2ElementNames.Count > 0)
+            {
+                identical = false;
+            }
+
+            foreach (string elementName in _allElementNames)
+            {
+                if (_missingModel1RelationProviderNames[elementName].Count > 0)
+                {
+                    identical = false;
+                }
+
+                if (_missingModel1RelationProviderNames[elementName].Count > 0)
+                {
+                    identical = false;
+                }
+            }
+            return identical;
+        }
+
+        private void UpdateProgress(string actionText, int currentItemCount, int totalItemCount, string itemType)
+        {
+            ProgressInfo progressInfo = new ProgressInfo();
+            progressInfo.ActionText = actionText;
+            progressInfo.CurrentItemCount = currentItemCount;
+            progressInfo.TotalItemCount = totalItemCount;
+            progressInfo.ItemType = itemType;
+            progressInfo.Percentage = currentItemCount * 100 / totalItemCount;
+            progressInfo.Done = currentItemCount == totalItemCount;
+            _progress?.Report(progressInfo);
+        }
+    }
+}
--- DsmSuite.Analyzer.Diff/Program.cs
+++ DsmSuite.Analyzer.Diff/Program.cs
@@ -1,15 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Reflection;
+using DsmSuite.Analyzer.Diff;
+using DsmSuite.Analyzer.Model.Core;
+using DsmSuite.Common.Util;
 
-namespace DsmSuite.Analyzer.Diff
+namespace DsmSuite.Analyzer.Cpp
 {
-    class Program
+    public class ConsoleAction : ConsoleActionBase
+    {
+        private readonly FileInfo _inputFile1;
+        private readonly FileInfo _inputFile2;
+
+        public ConsoleAction(FileInfo inputFile1, FileInfo inputFile2) : base("Comparing dependency files")
+        {
+            _inputFile1 = inputFile1;
+            _inputFile2 = inputFile2;
+        }
+
+        protected override bool CheckPrecondition()
+        {
+            return true;
+        }
+
+        protected override void LogInputParameters()
+        {
+            Logger.LogUserMessage("Input files:");
+            Logger.LogUserMessage($" {_inputFile1}");
+            Logger.LogUserMessage($" {_inputFile2}");
+        }
+
+        protected override void Action()
+        {
+            DsiModel model1 = new DsiModel("Diff", Assembly.GetExecutingAssembly());
+            DsiModel model2 = new DsiModel("Diff", Assembly.GetExecutingAssembly());
+            model1.Load(_inputFile1.FullName, this);
+            model2.Load(_inputFile2.FullName, this);
+            Comparer comparer = new Comparer(model1, model2, this);
+            comparer.Compare();
+        }
+
+        protected override void LogOutputParameters()
+        {
+        }
+    }
+
+    public static class Program
     {
         static void Main(string[] args)
         {
+            Logger.Init(Assembly.GetExecutingAssembly(), true);
+
+            if (args.Length < 2)
+            {
+                Logger.LogUserMessage("Usage: DsmSuite.Analyzer.Diff <file1> <file2>");
+            }
+            else
+            {
+                FileInfo inputFile1 = new FileInfo(args[0]);
+                FileInfo inputFile2 = new FileInfo(args[1]);
+                if (!inputFile1.Exists)
+                {
+                    Logger.LogUserMessage($"{inputFile1.FullName} does not exist.");
+                }
+                else if (!inputFile2.Exists)
+                {
+                    Logger.LogUserMessage($"{inputFile2.FullName} does not exist.");
+                }
+                else
+                {
+                    ConsoleAction action = new ConsoleAction(inputFile1, inputFile2);
+                    action.Execute();
+                }
+            }
         }
     }
 }
