00068febab085011afe87635658563e92aa1b1c8
jmuijsenberg <amina$1GH>
2020-09-22 07:04:02 +0200

    Added new rich tooltip viewmodels

---
+++ DsmSuite.DsmViewer.ViewModel/Matrix/ElementToolTipViewModel.cs
@@ -0,0 +1,36 @@
+﻿using DsmSuite.DsmViewer.ViewModel.Common;
+
+namespace DsmSuite.DsmViewer.ViewModel.Matrix
+{
+    public class ElementToolTipViewModel : ViewModelBase
+    {
+        private int _id;
+        private string _name;
+        private string _type;
+        private string _annotation;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; OnPropertyChanged(); }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; OnPropertyChanged(); }
+        }
+
+        public string Annotation
+        {
+            get { return _annotation; }
+            set { _annotation = value; OnPropertyChanged(); }
+        }
+    }
+}
---
+++ DsmSuite.DsmViewer.ViewModel/Matrix/RelationToolTipViewModel.cs
@@ -0,0 +1,51 @@
+﻿using DsmSuite.DsmViewer.ViewModel.Common;
+
+namespace DsmSuite.DsmViewer.ViewModel.Matrix
+{
+    public class RelationToolTipViewModel : ViewModelBase
+    {
+        private int _consumerId;
+        private string _consumerName;
+        private int _providerId;
+        private string _providerName;
+
+        private string _type;
+        private string _weight;
+
+        public int ConsumerId
+        {
+            get { return _consumerId; }
+            set { _consumerId = value; OnPropertyChanged(); }
+        }
+
+        public string ConsumerName
+        {
+            get { return _consumerName; }
+            set { _consumerName = value; OnPropertyChanged(); }
+        }
+
+        public int ProviderId
+        {
+            get { return _providerId; }
+            set { _providerId = value; OnPropertyChanged(); }
+        }
+
+        public string ProviderName
+        {
+            get { return _providerName; }
+            set { _providerName = value; OnPropertyChanged(); }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; OnPropertyChanged(); }
+        }
+
+        public int Weight
+        {
+            get { return _weight; }
+            set { _weight = value; OnPropertyChanged(); }
+        }
+    }
+}
