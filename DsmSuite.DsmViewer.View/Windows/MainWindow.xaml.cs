984e40df449addf78944c5911f8580509b2a19a0
jmuijsenberg <johan.van.den.muijsenberg@gmail.com>
2020-12-28 08:25:13 +0100

    Fixed screenshot when zoomed

--- DsmSuite.DsmViewer.View/Windows/MainWindow.xaml.cs
+++ DsmSuite.DsmViewer.View/Windows/MainWindow.xaml.cs
@@ -195,8 +195,8 @@ namespace DsmSuite.DsmViewer.View.Windows
 
         private void OnScreenshotRequested(object sender, System.EventArgs e)
         {
-            int width = (int)Matrix.UsedWidth;
-            int height = (int)Matrix.UsedHeight;
+            int width = (int) (Matrix.UsedWidth * _mainViewModel.ActiveMatrix.ZoomLevel);
+            int height = (int) (Matrix.UsedHeight * _mainViewModel.ActiveMatrix.ZoomLevel);
             RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
             renderTargetBitmap.Render(Matrix);
             Int32Rect rect = new Int32Rect(0, 24, width, height - 24);
