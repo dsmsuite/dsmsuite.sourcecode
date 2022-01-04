dfcd5c1fcfc38b3ccba83a17e4098a346d96d3de
jmuijsenberg <johan.van.den.muijsenberg@gmail.com>
2022-01-04 07:33:40 +0000

    Update relation editing

---
+++ DsmSuite.DsmViewer.View/UserControls/ElementSearchBox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DsmSuite.DsmViewer.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SearchBox.xaml
+    /// </summary>
+    public partial class ElementSearchBox : UserControl
+    {
+        public ElementSearchBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- DsmSuite.DsmViewer.ViewModel/Editing/Relation/RelationEditViewModel.cs
+++ DsmSuite.DsmViewer.ViewModel/Editing/Relation/RelationEditViewModel.cs
@@ -1,16 +1,19 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.Generic;
+using System.Windows.Input;
 using DsmSuite.DsmViewer.Application.Interfaces;
 using DsmSuite.DsmViewer.Model.Interfaces;
 using DsmSuite.DsmViewer.ViewModel.Common;
-
+using DsmSuite.DsmViewer.ViewModel.Main;
 
 namespace DsmSuite.DsmViewer.ViewModel.Editing.Relation
 {
     public class RelationEditViewModel : ViewModelBase
     {
         private readonly IDsmApplication _application;
-        private readonly IDsmRelation _relation; 
+        private readonly IDsmRelation _relation;
+        private string _selectedConsumerType;
         private readonly IDsmElement _consumer;
+        private string _providerConsumerType;
         private readonly IDsmElement _provider;
         private string _type;
         private int _weight;
@@ -22,9 +25,32 @@ namespace DsmSuite.DsmViewer.ViewModel.Editing.Relation
             _relation = relation;
             _consumer = consumer;
             _provider = provider;
+
+            ElementTypes = new List<string>();
+
+            ConsumerSearch = new ElementSearchViewModel(application);
+            ProviderSearch = new ElementSearchViewModel(application);
         }
 
         public string Title { get; }
+
+        public List<string> ElementTypes { get; }
+
+        public string SelectedConsumerType
+        {
+            get { return _selectedConsumerType; }
+            private set { _selectedConsumerType = value; OnPropertyChanged(); }
+        }
+
+        public ElementSearchViewModel ConsumerSearch { get; }
+
+        public string SelectedProviderType
+        {
+            get { return _providerConsumerType; }
+            private set { _providerConsumerType = value; OnPropertyChanged(); }
+        }
+
+        public ElementSearchViewModel ProviderSearch { get; }
 
         public string Help
         {
