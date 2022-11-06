5b0e7fd32f6513e3d1902d63bc9d047ead565c6b
jmuijsenberg <johan.van.den.muijsenberg@gmail.com>
2022-11-06 11:03:57 +0100

    Remove visual studio analyzer

--- DsmSuite.Analyzer.VisualStudio.Test.Data.DotNet/MyDotNetClass.cs
+++ DsmSuite.Analyzer.VisualStudio.Test.Data.DotNet/MyDotNetClass.cs
@@ -1,11 +1,9 @@
 ﻿using DsmSuiteAnalyzerVisualStudioTestDataClr;
-using DsmSuiteAnalyzerVisualStudioTestDataComLib;
 
 namespace DsmSuite.Analyzer.VisualStudio.Test.Data.DotNet
 {
     public class MyDotNetClass
     {
-        private ComA _a = new ComAClass();
         private MyClrClass _b = new MyClrClass();
     }
 }
