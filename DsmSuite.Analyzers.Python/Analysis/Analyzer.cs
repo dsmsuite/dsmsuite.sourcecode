8bd23a713c452e239e185ef60ed286b7a2010d0c
jmuijsenberg <johan.van.den.muijsenberg@gmail.com>
2025-04-23 06:46:36 +0200

    Improve python anallyzer

--- DsmSuite.Analyzers.Python/Analysis/Analyzer.cs
+++ DsmSuite.Analyzers.Python/Analysis/Analyzer.cs
@@ -62,7 +62,8 @@ namespace DsmSuite.Analyzers.Python.Analysis
     public class Analyzer
     {
         private readonly Dictionary<string, Module> _modules = new Dictionary<string, Module>();
-        private readonly IDsiModel _model;  
+        private readonly Dictionary<Module, Package> _moduleParentPackages = new Dictionary<Module, Package>();
+        private readonly IDsiModel _model;
         private readonly AnalyzerSettings _analyzerSettings;
         private string rootPackageName;
 
@@ -79,89 +80,105 @@ namespace DsmSuite.Analyzers.Python.Analysis
             {
                 string content = File.ReadAllText(_analyzerSettings.Input.JsonFilename);
                 DependencyRoot root = JsonSerializer.Deserialize<DependencyRoot>(content);
-    
-                foreach (var pkg in root.Packages)
+
+                foreach (var rootPackage in root.Packages)
                 {
-                    FindElements(pkg);
+                    FindElements(rootPackage);
                 }
 
-                foreach (var pkg in root.Packages)
-                    FindRelations(pkg);
+                foreach (var rootPackage in root.Packages)
+                {
+                    FindRelations(rootPackage);
+                }
             }
         }
 
-         void FindElements(Package pkg)
+        private void FindElements(Package package)
         {
-            foreach (Module module in pkg.Modules)
+            foreach (Module module in package.Modules)
             {
                 if (module.Name != "__init__")
                 {
-                    string name = $"{pkg.Name}.{module.Name}";
-                    _modules[name] = module;
-                    string elementName = $"{pkg.Name}.{module.Name}";
-
-                    _model.AddElement(elementName, "module", null);
+                    string moduleName = $"{package.Name}.{module.Name}";
+                    _modules[moduleName] = module;
+                    _moduleParentPackages[module] = package;
+                    Console.WriteLine($"Node {moduleName}");
+                    _model.AddElement(moduleName, "module", null);
                 }
             }
 
-            foreach (var sub in pkg.Packages)
+            foreach (var subPackage in package.Packages)
             {
-                FindElements(sub);
+                FindElements(subPackage);
             }
         }
 
-        void FindRelations(Package pkg)
+        private void FindRelations(Package package)
         {
-            foreach (var module in pkg.Modules)
-                FindRelations(pkg.Name, module);
+            foreach (Module module in package.Modules)
+                FindRelations(package.Name, module);
 
-            foreach (var sub in pkg.Packages)
-                FindRelations(sub);
+            foreach (Package subPackage in package.Packages)
+                FindRelations(subPackage);
         }
 
         void FindRelations(string packageName, Module module)
         {
-            foreach (var dep in module.Dependencies)
+            foreach (Dependency dep in module.Dependencies)
             {
-                // module info
-                //   "name": "{moduleName}>",
-                //   "path": "{sourcefolder}\\{packageName}\\{namespaceElement}\\{namespaceElement}\\{moduleName}py",
-
-                // dependency info
-                //   "target": "{namespace}.{moduleName}.{moduleName}",
-                // external seems always true so is not usable
-
-                int lastDot = dep.Target.LastIndexOf('.');
-                string depTarget = (lastDot != -1) ? dep.Target.Substring(0, lastDot) : dep.Target;
-
                 string consumerName = $"{packageName}.{module.Name}";
-                string providerName = $"{packageName}.{depTarget}";
-                Console.WriteLine($"Dep from {consumerName} to {providerName}");
 
-                if (!_modules.ContainsKey(consumerName))
-                {
-                    Logger.LogError($"Consumer element '{consumerName}' not found");
-                }
-                else if (!_modules.ContainsKey(providerName))
-                {
-                    string externalElementName = $"External.{dep.Target}";
-                    //_model.AddElement(externalElementName, "module", null);
+                Module? providerModule = FindProviderModule(dep);
 
-                    //RegisterRelation(consumerName, externalElementName);
+                if (providerModule != null)
+                {
+                    Package providerPackage = _moduleParentPackages[providerModule];
+                    string providerName = $"{providerPackage.Name}.{providerModule.Name}";
+
+                    if (!_modules.ContainsKey(consumerName))
+                    {
+                        Logger.LogError($"Consumer element '{consumerName}' not found");
+                    }
+                    else if (!_modules.ContainsKey(providerName))
+                    {
+                        Logger.LogError($"Provider element '{providerName}' not found");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Edge from {consumerName} to {providerName}");
+                        _model.AddRelation(consumerName, providerName, "dependency", 1, null);
+                    }
                 }
                 else
                 {
-                    RegisterRelation(consumerName, providerName);
+                    string providerName = $"External.{dep.Target}";
+                    _model.AddElement(providerName, "module", null);
+
+                    Console.WriteLine($"Edge from {consumerName} to {providerName}");
+                    _model.AddRelation(consumerName, providerName, "dependency", 1, null);
                 }
-            
             }
         }
 
-        private void RegisterRelation(string consumerName, string providerName)
+        private Module? FindProviderModule(Dependency dependency)
         {
-            _model.AddElement(consumerName, "", null);
-            _model.AddElement(providerName, "", null);
-            _model.AddRelation(consumerName, providerName, "dependency", 1, null);
+            Module? provider = null;
+
+            int lastDot = dependency.Target.LastIndexOf('.');
+            if (lastDot != -1)
+            {
+                string moduleName = dependency.Target.Substring(0, lastDot);
+
+                foreach (string key in _modules.Keys)
+                {
+                    if (key.EndsWith(moduleName))
+                    {
+                        provider = _modules[key];
+                    }
+                }
+            }
+
+            return provider;
         }
     }
 }
